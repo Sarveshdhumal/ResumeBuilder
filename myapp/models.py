f13d28c8e8e8c9380c45b3e64b594705a8530779
Sarvesh Dhumal <sarveshdhumal@Sarveshs-MacBook-Air.local>
2024-04-28 20:18:15 +0200

    [commit]

--- myapp/models.py
+++ myapp/models.py
@@ -32,5 +32,7 @@ class Resume(models.Model):
  job_city = models.CharField(max_length=50)
  profile_image = models.ImageField(upload_to='profileimg', blank=True)
  my_file = models.FileField(upload_to='doc', blank=True)
+ 
+ 
 
 
